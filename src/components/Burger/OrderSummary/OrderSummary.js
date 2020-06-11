a6e9b78df66fed9c30d4a0f080dd5374da881054
Damian Szczygielski <dszczygielski@farutex.pl>
2020-06-11 11:37:22 +0200

    restructure project

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,32 +1,38 @@
-import React from "react";
-import Aux from "../../../hoc/Aux";
+import React, { Component } from "react";
+import Aux from "../../../hoc/Aux/Aux";
 import Button from "../../UI/Button/Button";
 
-const orderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredients).map(
-    (ingredientKey) => {
-      return (
-        <li key={ingredientKey}>
-          <span style={{ textTransform: "capitalize" }}>{ingredientKey}</span>:{" "}
-          {props.ingredients[ingredientKey]}
-        </li>
-      );
-    }
-  );
+class OrderSummary extends Component {
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients).map(
+      (ingredientKey) => {
+        return (
+          <li key={ingredientKey}>
+            <span style={{ textTransform: "capitalize" }}>{ingredientKey}</span>
+            : {this.props.ingredients[ingredientKey]}
+          </li>
+        );
+      }
+    );
 
-  return (
-    <Aux>
-      <h3>Your Order</h3>
-      <p>An amazing burger with following ingredients:</p>
-      <ul>
-          {ingredientSummary}
-      </ul>
-      <p><strong>Total Price: {props.price} PLN</strong></p>
-      <p>Continue to checkout?</p>
-      <Button buttonType="Danger" clicked={props.canceled}>CANCEL</Button>
-      <Button buttonType="Success" clicked={props.continued}>CONTINUE</Button>
-    </Aux>
-  );
-};
+    return (
+      <Aux>
+        <h3>Your Order</h3>
+        <p>An amazing burger with following ingredients:</p>
+        <ul>{ingredientSummary}</ul>
+        <p>
+          <strong>Total Price: {this.props.price} PLN</strong>
+        </p>
+        <p>Continue to checkout?</p>
+        <Button buttonType="Danger" clicked={this.props.canceled}>
+          CANCEL
+        </Button>
+        <Button buttonType="Success" clicked={this.props.continued}>
+          CONTINUE
+        </Button>
+      </Aux>
+    );
+  }
+}
 
-export default orderSummary;
+export default OrderSummary;
